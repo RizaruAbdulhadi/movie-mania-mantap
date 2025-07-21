eee41ecd79c0d426147d4550502fee6a01d5e0f0
RizaruAbdulhadi <cerberus16121992@gmail.com>
2025-07-21 15:32:49 +0700

    Update 1
    Buang beberapa file yang tidak dibutuhkan dan merapikan file css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import MovieDetail from './pages/MovieDetail';
 import './App.css';
 
 function App() {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,6 +14,7 @@ import {
     getWatchedList
 } from '../services/storage';
 import './SidebarList.css';
+import './HomePage.css'
 
 const API_KEY = 'bd1056a1';
 
